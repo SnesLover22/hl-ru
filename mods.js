04968b24080154558bc46f886572d59e7395d029
Pixelsuft <yuskivl07@mail.ru>
2022-01-01 10:02:59 +0700

    updates

--- mods.js
+++ mods.js
@@ -1,5 +1,6 @@
 zipMods = [
-  ['halva.zip', 'Half-Life RUS (274M)', 78643200, 4, 274682334]
+  ['halva.zip', 'Half-Life RUS (274M)', 78643200, 4, 274682334],
+  ['halva_test_out.zip', 'Half-Life RUS (303M)', 78643200, 4, 303881453],
 ];
 
 pkgMods = [
